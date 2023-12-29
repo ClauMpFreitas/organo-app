e9d03e290cfb14b4c1fbeb829104dfe672a69f13
ClauMpFreitas <claudinhampfreitas@gmail.com>
2023-12-29 15:42:40 -0300

    Atualizado o caminho das imagens

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,30 +1,39 @@
 import "./Rodape.css";
+import fundoImagem from "../../imagens/fundo.png";
 
 const Rodape = () => {
+  const estiloRodape = {
+    backgroundImage: `url(${fundoImagem})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <footer className="main-footer">
-      <ul className="left-group">
-        <li>
-          <img src="imagens/fb.png" alt="facebook" />
-        </li>
-        <li>
-          <img src="imagens/ig.png" alt="instagram" />
-        </li>
-        <li>
-          <img src="imagens/tw.png" alt="twitter" />
-        </li>
-      </ul>
-      <ul className="center-group">
-        <li>
-          <img className="logo" src="/imagens/logo.png" alt="logo" />
-        </li>
-      </ul>
-      <ul className="right-group">
-        <li>
-          <h3>Desenvolvido por Alura e Cláudia</h3>
-        </li>
-      </ul>
-    </footer>
+    <section>
+      <footer className="main-footer" style={estiloRodape}>
+        <ul className="left-group">
+          <li>
+            <img src="imagens/fb.png" alt="facebook" />
+          </li>
+          <li>
+            <img src="imagens/ig.png" alt="instagram" />
+          </li>
+          <li>
+            <img src="imagens/tw.png" alt="twitter" />
+          </li>
+        </ul>
+        <ul className="center-group">
+          <li>
+            <img className="logo" src="/imagens/logo.png" alt="logo" />
+          </li>
+        </ul>
+        <ul className="right-group">
+          <li>
+            <h3>Desenvolvido por Alura e Cláudia</h3>
+          </li>
+        </ul>
+      </footer>
+    </section>
   );
 };
 
